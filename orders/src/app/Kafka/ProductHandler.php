c772faf1df6076897bceede77eb4a575bce78acb
Jonathan Xavier Ribeiro <jonathanxribeiro@gmail.com>
2020-12-23 14:13:18 -0300

    criando dados na tabela de produtos, no microserviço de order

--- orders/src/app/Kafka/ProductHandler.php
+++ orders/src/app/Kafka/ProductHandler.php
@@ -2,12 +2,27 @@
 
 namespace App\Kafka;
 
+use App\Models\Product;
 use PHPEasykafka\KafkaConsumerHandlerInterface;
 
 class ProductHandler implements KafkaConsumerHandlerInterface
 {
     public function __invoke(\RdKafka\Message $message, \RdKafka\KafkaConsumer $consumer)
     {
-        print_r($message->payload);
+        $payload = json_decode($message->payload);
+
+        $product = Product::firstOrCreate(
+            [ 'id' => $payload->id],
+            [
+                'name' => $payload->name,
+                'description' => $payload->description,
+                'price' => $payload->price,
+                'qtd_available' => $payload->qtd_available,
+                'qtd_total' => $payload->qtd_total
+            ]
+        );
+        
+
+        print_r($product->toJson());
     }
 }
